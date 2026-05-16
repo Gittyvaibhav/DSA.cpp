bf3749c4ab50546874db0745a1a88a8bb7c01f31
Gittyvaibhav <24bee103@nith.ac.in>
2026-05-16 15:53:23 +0530

    no

---
+++ RECURSION/striver/sumofsubsequence.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void subSequence(int arr[], int start, int n, vector<int> &ans, int target, int currentSum)
+{
+    // Base condition
+    if (start >= n)
+    {
+        if (currentSum == target)
+        {
+            cout << "{ ";
+
+            for (int x : ans)
+
+            {
+                cout << x << " ";
+            }
+
+            cout << "}" << endl;
+        }
+        return;
+    }
+
+    // Include current element
+    ans.push_back(arr[start]);
+    subSequence(arr, start + 1, n, ans, target, currentSum + arr[start]);
+
+    // Backtracking
+    ans.pop_back();
+
+    // Exclude current element
+    subSequence(arr, start + 1, n, ans, target, currentSum);
+}
+
+int main()
+{
+    int n;
+    int k;
+    cout << "ENTER THE TARGET SUM: ";
+    cin >> k;
+
+    cout << "ENTER THE NUMBER OF ELEMENTS: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "ENTER THE ELEMENTS: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    vector<int> ans;
+
+    cout << "\nAll Subsequences:\n";
+
+    subSequence(arr, 0, n, ans, k, 0);
+
+    return 0;
+}
