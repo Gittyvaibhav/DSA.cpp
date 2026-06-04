3710a445d12e97434fddf792f9f1bcb2cd211a88
Gittyvaibhav <24bee103@nith.ac.in>
2026-06-04 16:17:27 +0530

    updates

---
+++ binary Trees/hard/lca.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+int lca(Node *root, int p, int q)
+{
+    
+}
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    lca(root, 4, 5);
+}
