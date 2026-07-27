023488fe1c53b1ff932a516156c35f62338a76c2
Gittyvaibhav <24bee103@nith.ac.in>
2026-07-27 15:29:04 +0530

    scjobcica

---
+++ basics/patterns/rhombus.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 3;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 2 * n - i - 1; j++)
+        {
+            if (j < n - i - 1)
+                cout << " ";
+            else
+                cout << "*";
+        }
+
+        cout << endl;
+    }
+}
